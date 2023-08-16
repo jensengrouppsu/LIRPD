a73a2db58c8e4af07215982a71d3579bc8d4e819
Mark Bronson <mjb675@psu.edu>
2023-08-16 15:36:37 -0400

    Made changes to the parallization to work with windows and mac.

--- ex_integration.py
+++ ex_integration.py
@@ -11,72 +11,74 @@ An example code that runs the LIRPD method on a molecule's alpha and A-tensor de
 
 '''
 
-#########################
-#  Part I: Integration  #
-#########################
-
-# Choose how many processes to use in the integration for speeding up
-nproc = 4
-
-# Use any cube file for initialization
-cube_init = 'example-data/benzene/mode835.94-alpha-real-zz.cube'
-
-# Define field magnitude
-mr, mi = 25, 25
-# Define near field confinement
-b = 2.5  # angstrom
-bx, by, bz = b, b, b/2.0
-br = ANGSTROM2BOHR(np.array([bx,by,bz]))
-bi = ANGSTROM2BOHR(np.array([bx,by,bz]))
-# Define focal plane height
-height = ANGSTROM2BOHR(0.8)
-
-
-# Initialization for integration
-d = RamanDensityObj(cubefile=cube_init)
-height += d.coords[:,2].max()
-xpara = (d.coords[:,0].min()-5, d.coords[:,0].max()+5, 1)
-ypara = (d.coords[:,1].min()-5, d.coords[:,1].max()+5, 1)
-nx = int((xpara[1]-xpara[0])/xpara[2])+1
-ny = int((ypara[1]-ypara[0])/ypara[2])+1
-
-# Construct scanning grid. Store coordinates in scanxyz
-gridinfo = {}
-gridinfo['org'] = [xpara[0], ypara[0], height]
-gridinfo['stepsize'] = [xpara[2], ypara[2], 0]
-gridinfo['nstep'] = [nx, ny, 1]
-scanxyz = construct_grid(gridinfo)
-
-# Store withs and magnitude parameters for the field distribution
-d.field_params['mr'] = mr
-d.field_params['br'] = br
-d.field_params['mi'] = mi
-d.field_params['bi'] = bi
-
-# Perform local integration of Raman polarizability densities
-outfile = 'example-data/benzene/mode835.94.integrated.pickle' # path to the integrated density
-
-zzr = 'example-data/benzene/mode835.94-alpha-real-zz.cube'
-zzzr = 'example-data/benzene/mode835.94-aatensor-real-zzz.cube'
-zzxr = 'example-data/benzene/mode835.94-aatensor-real-zzx.cube'
-zzyr = 'example-data/benzene/mode835.94-aatensor-real-zzy.cube'
-d.collect_alpha_cube(zzr=zzr)
-print("Alpha-tensor collected!")
-d.collect_aatensor_cube(zzzr=zzzr, zzxr=zzxr, zzyr=zzyr)
-print("Aa-tensor collected!")
-
-# Scan the field while integrating the densities
-print('Integrating...')
-v_freq=835.94   # Need v_freq for this mode to calculate cross-section
-d.scan_constant_height(scanxyz, v_freq=v_freq, part='both', out=outfile, nproc=nproc)
-
-
-#########################
-#   Part II: Plotting   #
-#########################
-
-print('Plotting...')
-outfile = 'example-data/benzene/mode835.94.png'    
-integrated = np.load('example-data/benzene/mode835.94.integrated.pickle', allow_pickle=True)
-scanxyz = scanxyz.reshape(nx,ny,3)  # Reshape the scan coordinates for plotting
-plot_ters_image(scanxyz[:,:,0], scanxyz[:,:,1], integrated.reshape(nx,ny), d.coords, outfile)
+if __name__ == "__main__":
+    
+    #########################
+    #  Part I: Integration  #
+    #########################
+    
+    # Choose how many processes to use in the integration for speeding up
+    nproc = 4
+    
+    # Use any cube file for initialization
+    cube_init = 'example-data/benzene/mode835.94-alpha-real-zz.cube'
+    
+    # Define field magnitude
+    mr, mi = 25, 25
+    # Define near field confinement
+    b = 2.5  # angstrom
+    bx, by, bz = b, b, b/2.0
+    br = ANGSTROM2BOHR(np.array([bx,by,bz]))
+    bi = ANGSTROM2BOHR(np.array([bx,by,bz]))
+    # Define focal plane height
+    height = ANGSTROM2BOHR(0.8)
+    
+    
+    # Initialization for integration
+    d = RamanDensityObj(cubefile=cube_init)
+    height += d.coords[:,2].max()
+    xpara = (d.coords[:,0].min()-5, d.coords[:,0].max()+5, 1)
+    ypara = (d.coords[:,1].min()-5, d.coords[:,1].max()+5, 1)
+    nx = int((xpara[1]-xpara[0])/xpara[2])+1
+    ny = int((ypara[1]-ypara[0])/ypara[2])+1
+    
+    # Construct scanning grid. Store coordinates in scanxyz
+    gridinfo = {}
+    gridinfo['org'] = [xpara[0], ypara[0], height]
+    gridinfo['stepsize'] = [xpara[2], ypara[2], 0]
+    gridinfo['nstep'] = [nx, ny, 1]
+    scanxyz = construct_grid(gridinfo)
+    
+    # Store withs and magnitude parameters for the field distribution
+    d.field_params['mr'] = mr
+    d.field_params['br'] = br
+    d.field_params['mi'] = mi
+    d.field_params['bi'] = bi
+    
+    # Perform local integration of Raman polarizability densities
+    outfile = 'example-data/benzene/mode835.94.integrated.pickle' # path to the integrated density
+    
+    zzr = 'example-data/benzene/mode835.94-alpha-real-zz.cube'
+    zzzr = 'example-data/benzene/mode835.94-aatensor-real-zzz.cube'
+    zzxr = 'example-data/benzene/mode835.94-aatensor-real-zzx.cube'
+    zzyr = 'example-data/benzene/mode835.94-aatensor-real-zzy.cube'
+    d.collect_alpha_cube(zzr=zzr)
+    print("Alpha-tensor collected!")
+    d.collect_aatensor_cube(zzzr=zzzr, zzxr=zzxr, zzyr=zzyr)
+    print("Aa-tensor collected!")
+    
+    # Scan the field while integrating the densities
+    print('Integrating...')
+    v_freq=835.94   # Need v_freq for this mode to calculate cross-section
+    d.scan_constant_height(scanxyz, v_freq=v_freq, part='both', out=outfile, nproc=nproc)
+    
+    
+    #########################
+    #   Part II: Plotting   #
+    #########################
+    
+    print('Plotting...')
+    outfile = 'example-data/benzene/mode835.94.png'    
+    integrated = np.load('example-data/benzene/mode835.94.integrated.pickle', allow_pickle=True)
+    scanxyz = scanxyz.reshape(nx,ny,3)  # Reshape the scan coordinates for plotting
+    plot_ters_image(scanxyz[:,:,0], scanxyz[:,:,1], integrated.reshape(nx,ny), d.coords, outfile)
--- local_integration.py
+++ local_integration.py
@@ -1,6 +1,6 @@
 import numpy as np
 from field_models import field_lorentzian
-from multiprocessing import Process, Manager
+from multiprocessing import Process, Manager, Pool
 
 def scan_tip(alpha, aatensor, scanxyz, field_params, nproc=1):
     '''
@@ -15,30 +15,68 @@ def scan_tip(alpha, aatensor, scanxyz, field_params, nproc=1):
               (nscan, 3, 3)
     '''
 
-    manager = Manager()
-    shared_res = manager.list([[]]*nproc)
-    proc = []
+  # manager = Manager()
+  # shared_res = manager.list([[]]*nproc)
+  # proc = []
+  # scanchunk = np.array_split(scanxyz, nproc)
+
+  # def __worker(scanchunk, i):
+  #     tmp = manager.list(shared_res[i])
+  #     for tip in scanchunk:
+  #         field_params.update({'center': tip})
+  #         E, fg = field_lorentzian(**field_params)
+  #         tmp.append(_dressed_tensors(alpha, aatensor, E, fg))
+  #     shared_res[i] = tmp
+  #     #return res
+  # for i in range(nproc):
+  #     p = Process(target=__worker, args=(scanchunk[i], i))
+  #     p.daemon = True
+  #     proc.append(p)
+  #     p.start()
+  # for p in proc:
+  #     p.join()
+  # res = []
+  # for i in range(nproc):
+  #     res.extend(shared_res[i])
+  # print(res)
+  # return np.array(res)
+
+
+
     scanchunk = np.array_split(scanxyz, nproc)
 
-    def __worker(scanchunk, i):
-        tmp = manager.list(shared_res[i])
-        for tip in scanchunk:
-            field_params.update({'center': tip})
-            E, fg = field_lorentzian(**field_params)
-            tmp.append(_dressed_tensors(alpha, aatensor, E, fg))
-        shared_res[i] = tmp
-        #return res
+    inputs = []
     for i in range(nproc):
-        p = Process(target=__worker, args=(scanchunk[i], i))
-        p.daemon = True
-        proc.append(p)
-        p.start()
-    for p in proc:
-        p.join()
+        tmp = []
+        tmp.append(scanchunk[i])
+        tmp.append(alpha)
+        tmp.append(aatensor)
+        tmp.append(field_params)
+        inputs.append(tmp)
+
+
+    p = Pool(nproc)
+    output = p.map(worker, inputs)
+    
     res = []
     for i in range(nproc):
-        res.extend(shared_res[i])
+        res.extend(output[i])
+
     return np.array(res)
+    
+    
+
+def worker(inputs):
+    chunk = inputs[0]
+    alpha = inputs[1]
+    aatensor = inputs[2]
+    field_params = inputs[3]
+    tmp = []
+    for tip in chunk:
+        field_params.update({'center':tip})
+        E, fg = field_lorentzian(**field_params)
+        tmp.append(_dressed_tensors(alpha, aatensor, E, fg))
+    return tmp
 
 def _dressed_tensors(alpha, aatensor, E, fg):
     '''
